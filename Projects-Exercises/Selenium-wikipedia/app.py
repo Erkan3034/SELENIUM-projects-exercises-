11764e95eaab70da6f4db34bd78b05967bbe24d8
ERKAN TURGUT <159196857+Erkan3034@users.noreply.github.com>
2025-06-28 22:24:27 +0300

    app.py

--- Projects-Exercises/Selenium-wikipedia/app.py
+++ Projects-Exercises/Selenium-wikipedia/app.py
@@ -12,7 +12,7 @@ browser.get(url) # Sayfayı aç.
 
 time.sleep(5) # 10 saniye bekle sayfadayken
 
-elements = browser.find_elements(By.CSS_SELECTOR, "div.mw-heading.mw-heading3 > h3") # h3 etiketini seç
+elements = browser.find_elements(By.CSS_SELECTOR, "div.mw-heading.mw-heading3 > h3") # verilen class içindeki h3 etiketini seç
 
 i = 1    
 print("------------- BASLIKLAR-------------------")
